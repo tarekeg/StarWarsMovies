0e0eef23877b1eed73fe4f809eab3e270a83163f
Tarek <tarek.elghoul.ext@transway.fr>
2022-04-02 07:26:28 +0100

    Adding Movie Model

---
+++ StarWarsMovies/Models/Movie.swift
@@ -0,0 +1,20 @@
+//
+//  Movie.swift
+//  StarWarsMovies
+//
+//  Created by Tarek El Ghoul on 02/04/2022.
+//
+
+import Foundation
+
+struct Movie {
+    
+    var title: String?
+    var director: String?
+    var release_date: String?
+    var producer: String?
+    var opening_crawl: String?
+    var created: String?
+    var edited: String?
+    
+}
