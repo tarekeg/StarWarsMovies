c360a7077e226aa4b110fec32a397fc1ac428235
Tarek <tarek.elghoul.ext@transway.fr>
2022-04-02 07:29:26 +0100

    Adding MovieData Model

---
+++ StarWarsMovies/Models/MovieData.swift
@@ -0,0 +1,15 @@
+//
+//  MovieData.swift
+//  StarWarsMovies
+//
+//  Created by Tarek El Ghoul on 02/04/2022.
+//
+
+import Foundation
+
+struct MovieData {
+    
+    var count: Int?
+    var results: [Movie]?
+    
+}
